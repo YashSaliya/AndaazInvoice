19a94f432bf8fb5d9b21c4afe5e6fca0f532dbe2
Yash Saliya <79741768+YashSaliya@users.noreply.github.com>
2023-05-21 20:41:14 +0530

    Update settings.py

--- restInvoice/settings.py
+++ restInvoice/settings.py
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'vercel_app.wsgi.app'
+WSGI_APPLICATION = "restInvoice.wsgi.app"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
